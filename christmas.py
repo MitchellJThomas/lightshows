4e313a54a23d5bd5c2b5fd072920c958885f911e
Mitch Thomas <mthomas@tripwire.com>
2021-01-16 23:25:38 -0800

    Not sure why I'm calling it Christmas

--- christmas.py
+++ christmas.py
@@ -1,5 +1,9 @@
 from neo_pixel import color, gamma32
 
+# These are the computations to setup the LED values for
+# a red, white and blue flag motif with growing/diminishing intensity and a
+# theater chase effect (repeating pattern shifting left/right)
+
 intense = [60, 80, 100, 120, 140, 160, 180, 200, 255, 200, 180, 160, 140, 120, 100, 80, 60]
 def reds(intensity: int) -> int:
     return gamma32(color(intensity, 0, 0))
