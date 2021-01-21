df37e56afca9b73ea483fb31187ebdc67e9cb67e
Mitch Thomas <mthomas@tripwire.com>
2021-01-21 08:01:20 -0800

    Adding colors and factoring intensity

--- christmas.py
+++ christmas.py
@@ -4,12 +4,16 @@ from neo_pixel import color, gamma32
 # a red, white and blue flag motif with growing/diminishing intensity and a
 # theater chase effect (repeating pattern shifting left/right)
 
-rng = range(20, 255, 10)
-intense = list(rng) + [255] + list(reversed(rng))
+top = 200
+rng = range(40, top-20, 4)
+intense = list(rng) + [top] + list(reversed(rng))
 intense.pop()
 
 def reds(intensity: int) -> int:
     return gamma32(color(intensity, 0, 0))
+
+def yellows(intensity: int) -> int:
+    return gamma32(color(intensity, intensity, 0))
 
 def whites(intensity: int) -> int:
     return gamma32(color(intensity, intensity, intensity))
@@ -17,8 +21,19 @@ def whites(intensity: int) -> int:
 def blues(intensity: int) -> int:
     return gamma32(color(0, 0, intensity))
 
+def greens(intensity: int) -> int:
+    return gamma32(color(0, intensity, 0))
+
+def purples(intensity: int) -> int:
+    return gamma32(color(100, 10, intensity))
+
+def repeat(times: int, fn, *args):
+    return [fn(*args) for _ in range(times)]
+
 def flag(i: int) -> [int]:
-    return [reds(i), reds(i), reds(i), whites(i), whites(i), whites(i), blues(i), blues(i), blues(i)]
+    width = 6
+    return repeat(width, greens, i) + repeat(width, yellows, i) + repeat(width, purples, i)
+
 
 # Build C++ code for colors
 colors_str = str([flag(i) for i in intense]).replace('[', '{').replace(']', '}')
